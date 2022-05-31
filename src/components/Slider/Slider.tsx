19d5c426230af4db419f0c352ec8071ba05618e2
Tata Sysueva <t.sysueva031291@gmail.com>
2022-05-31 20:38:16 +1000

    refactor: change slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,38 +1,36 @@
 import React from 'react';
-import Swiper from 'react-id-swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
 import 'swiper/scss';
-import styles from './Slider.module.scss';
-import SwiperCore, { Navigation, Pagination, Controller, Thumbs } from 'swiper';
 import {BreedInfo} from '../../mocks/BreedInfo';
 
-SwiperCore.use([Navigation, Pagination, Controller, Thumbs]);
+import styles from './Slider.module.scss';
 
 interface SliderProps {
   infoBreed: BreedInfo,
 }
 
 function Slider({ infoBreed }: SliderProps) {
-  const params = {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      horizontalClass: styles.swiperPaginationHorizontal,
-      bulletActiveClass: styles.swiperBulletActive,
-      bulletClass: styles.swiperBullet,
-    },
-  };
-
   return (
-    <Swiper {...params} >
+    <Swiper
+      modules={[Pagination]}
+      spaceBetween={50}
+      pagination={{
+        clickable: true,
+        horizontalClass: styles.swiperPaginationHorizontal,
+        bulletActiveClass: styles.swiperBulletActive,
+        bulletClass: styles.swiperBullet,
+      }}
+    >
       {infoBreed.image
         .map(({url}) => (
-          <div key={Math.floor(Math.random() * (250 - 1 + 1)) + 1}>
+          <SwiperSlide key={Math.floor(Math.random() * (250 - 1 + 1)) + 1}>
             <img
               className={styles.image}
               src={url}
               alt={infoBreed.name}
             />
-          </div>
+          </SwiperSlide>
         ))}
     </Swiper>
   );
